9978b2c493daf2c7f9f1d8b9d1b1c5b98271303c
ENLYOWH <78600641+ENLYOWH@users.noreply.github.com>
2022-05-28 17:49:37 +0200

    updated

--- passeport_paris_rdv.py
+++ passeport_paris_rdv.py
@@ -11,17 +11,17 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
 data = {
-    "nombre"        : "4",
-    "time_in_secs"  : 9
+    "nombre"        : "4",      #Change values
+    "time_seconds"  : 5         #Change values
 }
+path = '.\chromedriver.exe'
+s = Service(path)
+browser = webdriver.Chrome(service=s, options= chrome_options)
 
+browser.get('https://teleservices.paris.fr/rdvtitres/jsp/site/Portal.jsp?page=appointmentsearch&view=search&category=titres')
+main_window = browser.current_window_handle
 while True:
-    path = 'D:\chromedriver_win32\chromedriver.exe'
-    s = Service(path)
-    browser = webdriver.Chrome(service=s, options= chrome_options)
 
-    browser.get('https://teleservices.paris.fr/rdvtitres/jsp/site/Portal.jsp?page=appointmentsearch&view=search&category=titres')
-    main_window = browser.current_window_handle
 
     name='nb_consecutive_slots'
     nombre = browser.find_element(by=By.NAME, value=name)
@@ -39,8 +39,8 @@ while True:
         break
     else:
         print("PAS DE RDV")
-        time.sleep(2)
-        browser.quit()
-        playsound('nope.wav')
-        time.sleep(data.get("time_in_secs"))
-        
+        time.sleep(1)
+        browser.refresh()
+        #browser.quit()             #close and restart a new chrome instance 
+        #playsound('nope.wav')      #play a sound when it fails
+        time.sleep(data.get("time_seconds"))
